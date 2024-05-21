29dfa9fb00e611f34f91dbb58a99ec5e4a769fc6
LuPow132 <68886157+LuPow132@users.noreply.github.com>
2024-05-21 23:45:19 +0700

    Create code.ino

---
+++ Accel/code.ino
@@ -0,0 +1,51 @@
+#include "FastIMU.h"
+#include <Wire.h>
+
+#define IMU_ADDRESS 0x69    //Change to the address of the IMU
+#define PERFORM_CALIBRATION //Comment to disable startup calibration
+BMI160 IMU;    
+calData calib = { 0 }; 
+AccelData accelData;    //Sensor data
+
+double AccX, AccY, AccZ;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(19200);
+  Wire.begin();
+  Wire.setClock(400000); //400khz clock
+  Serial.begin(115200);
+
+  int err = IMU.init(calib, IMU_ADDRESS);
+  if (err != 0) {
+    Serial.print("Error initializing IMU: ");
+    Serial.println(err);
+    while (true) {
+      ;
+    }
+  }
+  
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  IMU.update();
+  IMU.getAccel(&accelData);
+  
+  AccX = accelData.accelX;
+  AccY = accelData.accelY;
+  AccZ = accelData.accelZ;
+
+  Serial.print("AccX:");
+  Serial.print(AccX);
+  Serial.print(",");
+  Serial.print("AccY:");
+  Serial.print(AccY);
+  Serial.print(",");  
+  Serial.print("AccZ:");
+  Serial.print(AccZ);
+  Serial.println();
+
+  delay(10);
+}
