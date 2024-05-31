8807513ac9b52a7346524c771ffa228f9f7e51d3
LuPow132 <68886157+LuPow132@users.noreply.github.com>
2024-05-31 22:04:53 +0700

    Create Reciever.ino

---
+++ Accel/ESP-NOW/Reciever.ino
@@ -0,0 +1,66 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-one-to-many-esp32-esp8266/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+//Structure example to receive data
+//Must match the sender structure
+typedef struct dataPacket {
+  float AccX;
+  float AccY;
+  float AccZ;
+  float time;
+} dataPacket;
+
+//Create a struct_message called myData
+dataPacket packet;
+
+//callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&packet, incomingData, sizeof(dataPacket));
+  // Serial.print("Bytes received: ");
+  // Serial.print(len);
+  Serial.print("x:");
+  Serial.print(packet.AccX);
+  Serial.print(",");
+  Serial.print("y:");
+  Serial.print(packet.AccY);
+  Serial.print(",");
+  Serial.print("z:");
+  Serial.print(packet.AccZ);
+  Serial.print(",");
+  Serial.print("T:");
+  Serial.print(packet.time);
+  Serial.println();
+}
+ 
+void setup() {
+  //Initialize Serial Monitor
+  Serial.begin(115200);
+  
+  //Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+
+}
