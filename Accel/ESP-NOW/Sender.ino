a827b1eb3f3bc4574dd04090e1446b2ee541c101
LuPow132 <68886157+LuPow132@users.noreply.github.com>
2024-06-05 22:25:43 +0700

    Update Sender.ino

--- Accel/ESP-NOW/Sender.ino
+++ Accel/ESP-NOW/Sender.ino
@@ -5,15 +5,10 @@
 
 #define IMU_ADDRESS 0x69    //Change to the address of the IMU
 #define PERFORM_CALIBRATION //Comment to disable startup calibration
-BMI160 IMU;    
-calData calib = { 0 }; 
-AccelData accelData;    //Sensor data
-GyroData gyroData;
+BMI160 IMU;               //Change to the name of any supported IMU! 
 
-double AccX, AccY, AccZ;
-
-//ESP NOW CONFIG
 uint8_t broadcastAddress1[] = {0xE4, 0x65, 0xB8, 0x21, 0x43, 0x20};
+
 typedef struct dataPacket {
   float AccX;
   float AccY;
@@ -27,7 +22,13 @@ dataPacket packet;
 
 esp_now_peer_info_t peerInfo;
 
-// callback when data is sent
+// Currently supported IMUS: MPU9255 MPU9250 MPU6886 MPU6500 MPU6050 ICM20689 ICM20690 BMI055 BMX055 BMI160 LSM6DS3 LSM6DSL QMI8658
+
+calData calib = { 0 };  //Calibration data
+AccelData accelData;    //Sensor data
+GyroData gyroData;
+MagData magData;
+
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   char macStr[18];
   Serial.print("Packet to: ");
@@ -40,14 +41,11 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(19200);
   Wire.begin();
   Wire.setClock(400000); //400khz clock
   Serial.begin(115200);
-
   WiFi.mode(WIFI_STA);
- 
+
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
@@ -65,6 +63,10 @@ void setup() {
     return;
   }
 
+  while (!Serial) {
+    ;
+  }
+
   int err = IMU.init(calib, IMU_ADDRESS);
   if (err != 0) {
     Serial.print("Error initializing IMU: ");
@@ -74,15 +76,57 @@ void setup() {
     }
   }
   
-  delay(1000);
+#ifdef PERFORM_CALIBRATION
+  Serial.println("FastIMU calibration & data example");
+  delay(5000);
+  Serial.println("Keep IMU level.");
+  delay(5000);
+  IMU.calibrateAccelGyro(&calib);
+  Serial.println("Calibration done!");
+  Serial.println("Accel biases X/Y/Z: ");
+  Serial.print(calib.accelBias[0]);
+  Serial.print(", ");
+  Serial.print(calib.accelBias[1]);
+  Serial.print(", ");
+  Serial.println(calib.accelBias[2]);
+  Serial.println("Gyro biases X/Y/Z: ");
+  Serial.print(calib.gyroBias[0]);
+  Serial.print(", ");
+  Serial.print(calib.gyroBias[1]);
+  Serial.print(", ");
+  Serial.println(calib.gyroBias[2]);
+  delay(5000);
+  IMU.init(calib, IMU_ADDRESS);
+#endif
+
+  err = IMU.setGyroRange(500);      //USE THESE TO SET THE RANGE, IF AN INVALID RANGE IS SET IT WILL RETURN -1
+  err = IMU.setAccelRange(2);       //THESE TWO SET THE GYRO RANGE TO ±500 DPS AND THE ACCELEROMETER RANGE TO ±2g
+  
+  if (err != 0) {
+    Serial.print("Error Setting range: ");
+    Serial.println(err);
+    while (true) {
+      ;
+    }
+  }
 }
 
 void loop() {
-
-  // put your main code here, to run repeatedly:
   IMU.update();
-  IMU.getGyro(&gyroData);
   IMU.getAccel(&accelData);
+  IMU.getGyro(&gyroData);
+  // Serial.print(accelData.accelX);
+  // Serial.print("\t");
+  // Serial.print(accelData.accelY);
+  // Serial.print("\t");
+  // Serial.print(accelData.accelZ);
+  // Serial.print("\t");
+  // Serial.print(gyroData.gyroX);
+  // Serial.print("\t");
+  // Serial.print(gyroData.gyroY);
+  // Serial.print("\t");
+  // Serial.print(gyroData.gyroZ);
+  // Serial.println();
 
   packet.AccX = accelData.accelX;
   packet.AccY = accelData.accelY;
@@ -90,20 +134,6 @@ void loop() {
   packet.GyroX = gyroData.gyroX;
   packet.GyroY = gyroData.gyroY;
   packet.GyroZ = gyroData.gyroZ;
-  
-  Serial.print(packet.AccX);
-  Serial.print(",");
-  // Serial.print("y:");
-  Serial.print(packet.AccY);
-  Serial.print(",");
-  // Serial.print("z:");
-  Serial.print(packet.AccZ);
-  Serial.print(",");
-  Serial.print(packet.GyroX);
-  Serial.print(",");
-  Serial.print(packet.GyroY);
-  Serial.print(",");
-  Serial.print(packet.GyroZ);
 
   esp_err_t result = esp_now_send(0, (uint8_t *) &packet, sizeof(dataPacket));
    
@@ -113,4 +143,6 @@ void loop() {
   else {
     Serial.println("Error sending the data");
   }
+
+  delay(50);
 }
